11c28f7d6e31d05e8b7882531993153724bdcd52
faisalOmarBadaam <faisal.omar.badaam@gmail.com>
2024-04-01 10:33:33 +0300

    add singletone class and service folder and fix some bugs

---
+++ Jym_Management_System/Jym_Management_BussinessLayer/SingleUnitOFWork/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jym_Management_DataAccessLayer.Repositories;
+using Jym_Management_DataAccessLayer.Data;
+namespace Jym_Management_BussinessLayer.SingleUnitOFWork
+{
+    public sealed class UnitOfWorkBuissness
+    {
+        private static UnitOfWork _instance = null; 
+        private static AppDbContext _appDbContext= null;
+        private static readonly object _lock = new object();
+        private UnitOfWorkBuissness() { }
+       
+        public static UnitOfWork getInstenece()
+        {
+            if(_instance == null) 
+            { 
+                lock(_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _appDbContext = new AppDbContext();
+                        _instance = new UnitOfWork(_appDbContext);
+                        return _instance;
+                    }
+                        
+                }
+            }
+            return _instance;
+            
+
+        }
+
+    }
+}
