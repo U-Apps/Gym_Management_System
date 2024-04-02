cab28ec6f6baedbd06c993b61c63fd6d8edc5edf
Yousef-Binnob <yusufbinnob7111@gmail.com>
2024-04-03 00:59:11 +0300

    - added Identity library to DataAccessLayer - and created AppUser entity

---
+++ Jym_Management_System/Jym_Management_DataAccessLayer/Entities/Authentication/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Jym_Management_DataAccessLayer.Entities.Authentication;
+
+public class AppUser : IdentityUser
+{
+        public int PersonId { get; set; }
+        public bool IsActive { get; set; }
+        public virtual TbPerson Person { get; set; } = null!;
+}
