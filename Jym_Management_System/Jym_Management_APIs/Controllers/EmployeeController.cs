6abae98fbeaf72c858d82df1d27c41486fdb0403
faroog25x <faroogbajahaw25@gmail.com>
2024-04-03 22:43:11 +0300

    Add EmployeeController and register EmployeeService in Services

---
+++ Jym_Management_System/Jym_Management_APIs/Controllers/EmployeeController.cs
@@ -0,0 +1,73 @@
+﻿using Jym_Management_BussinessLayer.Modules;
+using Jym_Management_BussinessLayer.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jym_Management_APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeService _employeeService;
+
+        public EmployeeController(EmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public ActionResult<int> CreateEmployee(Employee employee)
+        {
+            _employeeService.Add(employee);
+            return Ok(employee.EmployeeId);
+        }
+
+        [HttpPut]
+        [Route("")]
+        public ActionResult UpdateEmployee(Employee employee)
+        {
+            var existingEmployee=_employeeService.GetById(employee.EmployeeId);
+            if (existingEmployee == null)
+                return NotFound();
+
+            existingEmployee.HireDate = employee.HireDate;
+            existingEmployee.ResignationDate = employee.ResignationDate;
+            existingEmployee.Salary = employee.Salary;
+            existingEmployee.PersonId = employee.PersonId;
+
+            _employeeService.Update(existingEmployee);
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult<IEnumerable<Employee>> Get()
+        {
+            IEnumerable<Employee> employees = _employeeService.GetAll();
+            return Ok(employees);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<Employee> GetById(int id)
+        {
+            Employee employee = _employeeService.GetById(id);
+            return employee is null ? NotFound() : Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public ActionResult Delete(int id)
+        {
+            Employee employee = _employeeService.GetById(id);
+
+            if (employee is null)
+                return NotFound();
+
+            _employeeService.DeleteById(id);
+            return Ok();
+        }
+    }
+}
