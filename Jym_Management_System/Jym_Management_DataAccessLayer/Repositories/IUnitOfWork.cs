49c9a13cbcd882d36866d47008b827a956167909
faroog25x <faroogbajahaw25@gmail.com>
2024-04-04 17:04:32 +0300

    Completing Repositories in UnitOfWork

--- Jym_Management_System/Jym_Management_DataAccessLayer/Repositories/IUnitOfWork.cs
+++ Jym_Management_System/Jym_Management_DataAccessLayer/Repositories/IUnitOfWork.cs
@@ -12,6 +12,17 @@ namespace Jym_Management_DataAccessLayer.Repositories
         public IBaseRepository<TbMember> Members { get;}
         public IBaseRepository<TbUser> Users { get;}
         public IBaseRepository<TbEmployee> Employees { get;}
+        public IBaseRepository<TbExerciseType> ExerciseTypes { get; }
+        public IBaseRepository<TbJob> Jobs { get; }
+        public IBaseRepository<TbPayrollPayment> PayrollPayments { get; }
+        public IBaseRepository<TbJobHistory> JobHistories { get; }
+        public IBaseRepository<TbPeriod> Periods { get; }
+        public IBaseRepository<TbPermssion> Permssions { get; }
+        public IBaseRepository<TbPerson> Persons { get; }
+        public IBaseRepository<TbRole> Roles { get; }
+        public IBaseRepository<TbSubsciptionPeriod> SubsciptionPeriods { get; }
+        public IBaseRepository<TbSubscription> Subscriptions { get; }
+        public IBaseRepository<TbSubscriptionPayment> SubscriptionPayments { get; }
 
         public int Complete();
 
