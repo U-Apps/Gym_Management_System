559a18b2c766b1cedf5605f2e37ae7d308cd8440
faroog25x <faroogbajahaw25@gmail.com>
2024-04-07 00:19:21 +0300

    Complete all DTOs

---
+++ Jym_Management_System/Jym_Management_APIs/DTO_modules/PermssionDTO.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Jym_Management_APIs.DTO_modules
+{
+    public record ReadPermssionDTO
+        (
+              byte Id ,
+              string? Name,
+
+              ReadRoleDTO? Role
+
+
+        );
+
+    public record CreatePermssionDTO
+       (
+              
+              string? Name,
+
+              [Required]
+              byte? RoleId
+
+
+       );
+
+    public record UpdatePermssionDTO
+      (
+            [Required]
+            byte Id,
+
+            string? Name,
+            [Required]
+            byte? RoleId
+
+      );
+}
---
+++ Jym_Management_System/Jym_Management_APIs/DTO_modules/RoleDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Jym_Management_APIs.DTO_modules
+{
+    public record ReadRoleDTO(byte RoleId ,string RoleName);
+
+    public record CreateRoleDTO (string RoleName);
+
+    public record UpdateRoleDTO ([Required] byte RoleId ,string RoleName);
+}
