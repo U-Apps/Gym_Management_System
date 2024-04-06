3c27c953b601f814928d3236bfcdbe813deb81a6
faisalOmarBadaam <faisal.omar.badaam@gmail.com>
2024-04-06 13:05:34 +0300

    create and uplode DTO for Employee and Person

---
+++ Jym_Management_System/Jym_Management_APIs/DTO_modules/EmployeeDTO.cs
@@ -0,0 +1,48 @@
+﻿using Jym_Management_BussinessLayer.Modules;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Jym_Management_APIs.DTO_modules
+{
+    public record ReadEmployeeDTO
+    {
+       
+        public int PersonId { get; set; }
+        public DateTime HireDate { get; set; }
+        public DateTime ResignationDate { get; set; }
+        public decimal Salary { get; set; }
+
+        ReadPersonDTO person { get; set; }
+
+    }
+    public record UpdateEmployeeDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int PersonId { get; set; }
+        public DateTime HireDate { get; set; }
+        public DateTime ResignationDate { get; set; }
+        [Required]
+
+        public decimal Salary { get; set; }
+
+
+    }
+
+    public record CreateEmployeeDTO
+    {
+        [Required]
+
+        public int PersonId { get; set; }
+       
+
+        public DateTime HireDate { get; set; }
+        public DateTime ResignationDate { get; set; }
+        [Required]
+
+        public decimal Salary { get; set; }
+
+
+    }
+}
