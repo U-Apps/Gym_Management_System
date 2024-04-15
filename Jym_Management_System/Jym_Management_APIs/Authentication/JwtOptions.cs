f1641c963e9bb0b90279c54eaa3b2b2b80cd4da2
Yousef-Binnob <yusufbinnob7111@gmail.com>
2024-04-16 01:15:41 +0300

    [WIP] Added Classes that are responsible for authenticating users

---
+++ Jym_Management_System/Jym_Management_APIs/Authentication/JwtOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Jym_Management_APIs.Authentication
+{
+    public class JwtOptions
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string Lifetime { get; set; }
+        public string SigningKey { get; set; }
+    }
+}
---
+++ Jym_Management_System/Jym_Management_APIs/DTO_modules/LoginDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Jym_Management_APIs.DTO_modules
+{
+    public record LoginDto
+        (
+            [Required]
+            string UserName,
+            [Required]
+            string Password
+        );
+    
+    public record LoginResponseDto
+        (
+            [Required]
+            bool LoginResult,
+            string? Token
+        );
+}
