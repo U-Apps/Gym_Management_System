6b9e62a7fbbb10f496514fe4bfd5e1505950439d
Yousef-Binnob <yusufbinnob7111@gmail.com>
2024-04-16 01:10:08 +0300

    [WIP] Added Authentication controller

---
+++ Jym_Management_System/Jym_Management_APIs/Controllers/Authentication.cs
@@ -0,0 +1,27 @@
+﻿using Jym_Management_APIs.Authentication;
+using Jym_Management_APIs.DTO_modules;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jym_Management_APIs.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Authentication : ControllerBase
+    {
+        private LoginManager _LoginManager { get; }
+        public Authentication(LoginManager loginManager)
+        {
+            _LoginManager = loginManager;
+        }
+
+
+        [HttpPost]
+        [Route("")]
+        public ActionResult<LoginResponseDto> Login(LoginDto Credentials)
+        {
+            return _LoginManager.Login(Credentials);
+        }
+
+        
+    }
+}
