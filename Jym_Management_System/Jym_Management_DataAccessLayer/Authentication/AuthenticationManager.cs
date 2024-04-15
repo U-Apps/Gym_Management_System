a192a18e5cfb8ec226af5fe4e01f14de93c87329
Yousef-Binnob <yusufbinnob7111@gmail.com>
2024-04-15 17:29:29 +0300

    added the AuthenticationManager class in the data access layer that include a method to log in a user, i did this because the passowr is hashed and can validate it only via UserManager class

---
+++ Jym_Management_System/Jym_Management_DataAccessLayer/Authentication/AuthenticationManager.cs
@@ -0,0 +1,33 @@
+﻿using Jym_Management_DataAccessLayer.Entities.Authentication;
+using Jym_Management_DataAccessLayer.Repositories;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jym_Management_DataAccessLayer.Authentication
+{
+    internal class AuthenticationManager
+    {
+        private UserManager<AppUser> _UserManager { get; }
+
+        public AuthenticationManager()
+        {
+            _UserManager = ServiceConfiguration.GetService<UserManager<AppUser>>();
+        }
+
+        public AppUser? Login(string UserName, string Password)
+        {
+            var User = _UserManager.FindByNameAsync(UserName).Result;
+            if (User == null)
+                return null;
+
+            if (!_UserManager.CheckPasswordAsync(User, Password).Result)
+                return null;
+            
+            return User;
+        }
+    }
+}
