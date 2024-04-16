ef1b3a53ca34842a35fad08c96181c3205c2dea3
Saleh_Mohammed <salhbnsmyd3@gmail.com>
2024-04-17 00:48:02 +0300

    tbSubscriptionPayment
    setting configuration for subsciption payemtn class

--- Jym_Management_System/Jym_Management_DataAccessLayer/Data/Config/TbSubscriptionPaymentConfiguration.cs
+++ Jym_Management_System/Jym_Management_DataAccessLayer/Data/Config/TbSubscriptionPaymentConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Jym_Management_DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,36 @@ using System.Threading.Tasks;
 
 namespace Jym_Management_DataAccessLayer.Data.Config
 {
-    internal class TbSubscriptionPaymentConfiguration
+    internal class TbSubscriptionPaymentConfiguration : IEntityTypeConfiguration<TbSubscriptionPayment>
     {
+        public void Configure(EntityTypeBuilder<TbSubscriptionPayment> builder)
+        {
+            builder.HasKey(e => e.PaymentId);
+
+            builder.Property(e => e.PaymentId)
+                .UseIdentityColumn(1);
+
+            builder.ToTable("tbSubscriptionPayments");
+
+            builder.Property(e => e.CreatedByUserId).HasColumnName("CreatedByUserID");
+
+            builder.Property(e => e.PaymentAmount).HasColumnType("smallmoney");
+
+            builder.Property(e => e.PaymentDate).HasColumnType("date");
+
+            builder.Property(e => e.PaymentId).HasColumnName("PaymentID");
+
+            builder.Property(e => e.SubscriptionId).HasColumnName("SubscriptionID");
+
+            builder.HasOne(d => d.CreatedByUser)
+                .WithMany()
+                .HasForeignKey(d => d.CreatedByUserId)
+                .HasConstraintName("FK_tbSubscriptionPayments_AspNetUsers");
+
+            builder.HasOne(d => d.Subscription)
+                .WithMany()
+                .HasForeignKey(d => d.SubscriptionId)
+                .HasConstraintName("FK_tbSubscriptionPayments_tbSubscriptions");
+        }
     }
 }
