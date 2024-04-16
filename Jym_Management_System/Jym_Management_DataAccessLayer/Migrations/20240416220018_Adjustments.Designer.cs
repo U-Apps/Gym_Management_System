f20b8176e44f3b0051bd111ef73179fabb1bd85f
Yousef-Binnob <yusufbinnob7111@gmail.com>
2024-04-17 01:22:32 +0300

    - Added a migration which adjusted the data type of weight column - adjusted the connection string - updated the database

--- Jym_Management_System/Jym_Management_DataAccessLayer/Migrations/20240416220018_Adjustments.Designer.cs
+++ Jym_Management_System/Jym_Management_DataAccessLayer/Migrations/20240416220018_Adjustments.Designer.cs
@@ -4,6 +4,7 @@ using Jym_Management_DataAccessLayer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Jym_Management_DataAccessLayer.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240416220018_Adjustments")]
+    partial class Adjustments
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
