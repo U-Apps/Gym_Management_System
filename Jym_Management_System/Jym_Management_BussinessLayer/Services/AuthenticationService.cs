7f51db2a2788e8ac7c7d3f86121d86912a72ddb7
Yousef-Binnob <yusufbinnob7111@gmail.com>
2024-04-17 00:58:16 +0300

    added AuthenticationService in business layer

---
+++ Jym_Management_System/Jym_Management_BussinessLayer/Services/AuthenticationService.cs
@@ -0,0 +1,29 @@
+﻿using Jym_Management_BussinessLayer.AutoMapper;
+using Jym_Management_BussinessLayer.Modules;
+using Jym_Management_DataAccessLayer;
+using Jym_Management_DataAccessLayer.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jym_Management_BussinessLayer.Services
+{
+    internal class AuthenticationService
+    {
+        AuthenticationManager Manager { get; }
+
+        public AuthenticationService()
+        {
+            Manager = ServiceConfiguration.GetService<AuthenticationManager>();
+        }
+
+        public User Login(string UserName, string Password)
+        {
+            var user = Manager.Login(UserName, Password);
+
+            return user == null ? null : Mapping.Mapper.Map<User>(user);
+        }
+    }
+}
