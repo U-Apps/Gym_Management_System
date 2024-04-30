9f5cdec2bc6d59f997382b992e6c4c6b8c4e00cb
Saleh Mohammed <salhbnsmyd3@gmail.com>
2024-04-30 20:14:46 +0300

    System Roles > Added

---
+++ Jym_Management_System/Jym_Management_APIs/Authentication/SystemRoles.cs
@@ -0,0 +1,10 @@
+﻿namespace Jym_Management_APIs.Authentication
+{
+    public record SystemRoles
+    {
+        const string Admin = "Admin";
+        const string User = "User"; // could be Coach or Accountant
+        const string Member = "Member";
+
+    }
+}
