07a0232cb7b4e94dfcd5e8540f8203c6e2c53ac3
Yousef-Binnob <yusufbinnob7111@gmail.com>
2024-09-07 14:55:06 +0300

    moved the data access services's registeration code from ServicesConfiguration.cs to the DependencyInjection.cs

--- GymManagementSystem/GymManagement.DataAccess/Configuration/DependencyInjection.cs
+++ GymManagementSystem/GymManagement.DataAccess/Configuration/DependencyInjection.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using GymManagement.BusinessCore.Models;
+using GymManagement.DataAccess.Data;
+using GymManagement.DataAccess.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace GymManagement.DataAccess.Configuration
 {
@@ -6,8 +11,47 @@ namespace GymManagement.DataAccess.Configuration
     {
         public static IServiceCollection AddDataAccess(this IServiceCollection services)
         {
+            services.AddEntityFrameworkCoreServices()
+                    .AddIdentityServices()
+                    .AddRepositories();
 
             return services;
         }
+
+        private static IServiceCollection AddEntityFrameworkCoreServices(this IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>((options) =>
+            {
+                options.UseSqlServer(@"Server=FMSI\SQLEXPRESS;Database=JymManagementSystem;Integrated Security=SSPI;");
+            });
+
+            return services;
+        }
+
+        private static IServiceCollection AddIdentityServices(this IServiceCollection services)
+        {
+            services.AddIdentityCore<AppUser>()
+                .AddRoles<IdentityRole<int>>()
+                .AddEntityFrameworkStores<AppDbContext>();
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 0;
+            });
+
+            return services;
+        }
+
+        private static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<UserRepository>();
+
+            return services;
+        }
+
     }
 }
