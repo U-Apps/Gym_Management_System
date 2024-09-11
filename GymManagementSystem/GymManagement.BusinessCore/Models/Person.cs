3646fc8c3301058ddae55049b54c43a7d773d604
Yousef-Binnob <yusufbinnob7111@gmail.com>
2024-09-11 05:50:12 +0300

    - cleaned up properties in person and empployee - make the employee inherits from person

--- GymManagementSystem/GymManagement.BusinessCore/Models/Person.cs
+++ GymManagementSystem/GymManagement.BusinessCore/Models/Person.cs
@@ -1,18 +1,45 @@
 ﻿
 namespace GymManagement.BusinessCore.Models
 {
-    public class Person
+    public abstract class Person
     {
-        
-        public int PersonId { get; set; }
-        public string? Idcard { get; set; }
-        public string Name { get; set; } = null!;
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string SecondName { get; set; } = null!;
+        public string ThirdName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? NationalNumber { get; set; }
         public string? PhoneNumber { get; set; }
-        public DateTime BirthDate { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public short Age  => (short)(DateTime.Now.Subtract(BirthDate.Value).TotalDays/365);
         public string? Email { get; set; }
+        public string? Address { get; set; }
+        public DateTime RegisterationDate { get; set; }
 
-        public virtual Employee Employee { get; set; }
-        public virtual Member Member { get; set; }
-        public virtual AppUser? User { get; set; }
+        //public Person()
+        //{
+        //    Id = -1;
+        //    FirstName = SecondName = ThirdName = LastName = "Unknown";
+        //    RegisterationDate = DateTime.Now;
+        //    NationalNumber = PhoneNumber = Email = Address = null;
+        //    BirthDate = null;
+        //}
+
+        //public Person(int id, string firstName, string secondName,
+        //    string thirdName, string lastName, string? nationalNumber,
+        //    string? phoneNumber, DateTime? birthDate, string? email, string? address, DateTime registerationDate)
+        //{
+        //    Id = id;
+        //    FirstName = firstName;
+        //    SecondName = secondName;
+        //    ThirdName = thirdName;
+        //    LastName = lastName;
+        //    NationalNumber = nationalNumber;
+        //    PhoneNumber = phoneNumber;
+        //    BirthDate = birthDate;
+        //    Email = email;
+        //    Address = address;
+        //    RegisterationDate = registerationDate;
+        //}
     }
 }
