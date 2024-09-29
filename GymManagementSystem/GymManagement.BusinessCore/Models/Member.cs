20fc3461cbd731aa2bf028c1457fe9b69843283d
Yousef-Binnob <yusufbinnob7111@gmail.com>
2024-09-29 11:10:29 +0300

    - make the Member inherits from Person - make IsActive property required - reconfigured Member entity to map to database

--- GymManagementSystem/GymManagement.BusinessCore/Models/Member.cs
+++ GymManagementSystem/GymManagement.BusinessCore/Models/Member.cs
@@ -2,14 +2,11 @@
 namespace GymManagement.BusinessCore.Models
 
 {
-    public class Member
+    public class Member : Person
     {
-        public int MemberId { get; set; }
-        public int PersonId { get; set; }
         public decimal? MemberWeight { get; set; }
-        public bool? IsActive { get; set; }
+        public bool IsActive { get; set; }
 
-        public virtual Person Person { get; set; } = null!;
         public virtual ICollection<Subscription> Subscriptions { get; set; }
     }
 }
