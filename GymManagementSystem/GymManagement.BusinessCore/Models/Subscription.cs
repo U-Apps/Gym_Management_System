9f1a4240e5efdbea8472362f6149ff7b4ace0c9c
Yousef-Binnob <yusufbinnob7111@gmail.com>
2024-09-30 10:14:24 +0300

    - adjusted the nallability of some properties in Subscription and its confuguration. - added SubscriptionPayment as navigation property in Subscription. - explicit the data type and relationships restrictions more specifically in Subscription configuration

--- GymManagementSystem/GymManagement.BusinessCore/Models/Subscription.cs
+++ GymManagementSystem/GymManagement.BusinessCore/Models/Subscription.cs
@@ -5,19 +5,20 @@ namespace GymManagement.BusinessCore.Models
     {
         public int SubscriptionId { get; set; }
         public int MemberId { get; set; }
-        public int CoachId { get; set; }
+        public int? CoachId { get; set; }
         public int CreatedByReceptionistId { get; set; }
         public byte ExcerciseTypeId { get; set; }
         public byte PeriodId { get; set; }
-        public DateTime? StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
         public byte SubscriptionPeriodId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
 
-        public virtual JobHistory Coach { get; set; } = null!;
-        public virtual JobHistory CreatedByReceptionist { get; set; } = null!;
-        public virtual ExerciseType ExcerciseType { get; set; } = null!;
         public virtual Member Member { get; set; } = null!;
+        public virtual JobHistory? Coach { get; set; }
+        public virtual JobHistory CreatedByReceptionist { get; set; } = null!;
+        public virtual ExerciseType ExcerciseType { get; set; }
         public virtual Period Period { get; set; } = null!;
         public virtual SubscriptionPeriod SubscriptionPeriod { get; set; } = null!;
+        public virtual SubscriptionPayment? SubscriptionPayment { get; set; }
     }
 }
