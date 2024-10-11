295c68b814bac5acb44d6cbd52811319d209a0cd
Yousef-Binnob <yusufbinnob7111@gmail.com>
2024-10-11 22:20:14 +0300

    implemented getting the subscription period by id

---
+++ GymManagementSystem/GymManagement.DataAccess/Repositories/SubscriptionPeriodRepo.cs
@@ -0,0 +1,20 @@
+﻿using GymManagement.BusinessCore.Contracts.Repositories;
+using GymManagement.BusinessCore.Models;
+using GymManagement.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagement.DataAccess.Repositories
+{
+    class SubscriptionPeriodRepo(AppDbContext _appDbContext) : ISubscriptionPeriodRepo
+    {
+        public SubscriptionPeriod GetPeriodById(int periodId)
+        {
+            return _appDbContext.Set<SubscriptionPeriod>().FirstOrDefault(s => s.Id == periodId);
+        }
+    }
+}
