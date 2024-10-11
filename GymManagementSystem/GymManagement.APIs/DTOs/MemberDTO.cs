390a9bbb40e0c7bb47ae0d93ce70b02fc6de5096
Yousef-Binnob <yusufbinnob7111@gmail.com>
2024-10-11 22:48:15 +0300

    created a DTO for creating a new member

--- GymManagementSystem/GymManagement.APIs/DTOs/MemberDTO.cs
+++ GymManagementSystem/GymManagement.APIs/DTOs/MemberDTO.cs
@@ -4,35 +4,46 @@ namespace GymManagement.APIs.DTOs
 {
     public record ReadMemberDTO 
         (
-              int MemberId,
-              //int PersonId ,
-              decimal? MemberWeight ,
-              bool IsActive 
-
-              //ReadPersonDTO Person
-        
+            int MemberId,
+            string FirstName,
+            string SecondName,
+            string ThirdName,
+            string LastName,
+            string? NationalNumber,
+            string? PhoneNumber,
+            DateTime? BirthDate,
+            string? Address,
+            string? Email,
+            decimal? MemberWeight,
+            bool IsActive,
+            DateTime RegisterationDate
         );
 
     public record CreateMemberDTO
        (
             [Required]
+             string FirstName,
+            [Required]
+             string SecondName,
+             string ThirdName,
+             [Required]
+             string LastName,
+            [Required]
             [DataType(DataType.CreditCard)]
-             string? Idcard,
-            [Required]
-            [StringLength(50, MinimumLength = 6)]
-            string Name,
-            [Required]
+             string? NationalNumber,
+
             [DataType(DataType.PhoneNumber)]
             string? PhoneNumber,
-            [Required]
             [DataType(DataType.Date)]
-            DateTime BirthDate,
+            DateTime? BirthDate,
+            string? Address,
             [Required]
             [EmailAddress]
             string? Email,
-             [Range(1,200)]
-             decimal? MemberWeight,
-             bool IsActive
+            [Range(1,200)]
+            decimal? MemberWeight,
+            [Required]
+            CreateSubscriptionInfo SubscriptionInfo
 
        );
 
