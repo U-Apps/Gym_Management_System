0f44cbf88ef40e236aa6910e4ca89044c776db76
Yousef-Binnob <yusufbinnob7111@gmail.com>
2024-10-11 22:39:27 +0300

    implemented adding new subscription service

---
+++ GymManagementSystem/GymManagement.BusinessCore/Contracts/Services/ISubscriptionService.cs
@@ -0,0 +1,14 @@
+﻿using GymManagement.BusinessCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagement.BusinessCore.Contracts.Services
+{
+    public interface ISubscriptionService
+    {
+        bool AddNewSubscription(Subscription model);
+    }
+}
---
+++ GymManagementSystem/GymManagement.BusinessCore/Services/SubscriptionService.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper.Execution;
+using GymManagement.BusinessCore.Contracts.Repositories;
+using GymManagement.BusinessCore.Contracts.Services;
+using GymManagement.BusinessCore.Models;
+
+namespace GymManagement.BusinessCore.Services
+{
+    public class SubscriptionService(ISubscriptionRepo _subscriptionRepo
+        ,ISubscriptionPeriodRepo _subscriptionPeriodRepo) : ISubscriptionService
+    {
+        
+        public bool AddNewSubscription(Subscription subscription)
+        {
+            if ( subscription.ExcerciseTypeId == 0
+                || subscription.PeriodId == 0
+                || subscription.SubscriptionPeriodId == 0) 
+            {
+                return false;
+            }
+
+            // here must join the subscription with the user create it
+            // not implemented yet .....
+
+            subscription.StartDate = DateTime.Now;
+            var SubscriptionPeriod = _subscriptionPeriodRepo.GetPeriodById(subscription.PeriodId);
+            if (SubscriptionPeriod == null)
+            {
+                return false;
+            }
+            subscription.EndDate = subscription.StartDate.AddDays(SubscriptionPeriod.PeriodDays);
+            _subscriptionRepo.AddNewSubscription(subscription);
+            return true;
+        }
+
+    }
+}
