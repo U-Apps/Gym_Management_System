7106a9ab3c59ea400271c9da2c9cf0836fd863e8
Yousef-Binnob <yusufbinnob7111@gmail.com>
2024-10-11 22:51:48 +0300

    implemented creating a new member api endpoint

--- GymManagementSystem/GymManagement.APIs/Controllers/MemberController.cs
+++ GymManagementSystem/GymManagement.APIs/Controllers/MemberController.cs
@@ -4,108 +4,85 @@ using Microsoft.AspNetCore.Mvc;
 using GymManagement.BusinessCore.Contracts.Services;
 using Microsoft.AspNetCore.Authorization;
 using GymManagement.APIs.Authentication;
+using GymManagement.APIs.DTOs.Mappers;
 
 namespace GymManagement.APIs.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(Roles = (clsSystemRoles.User + "," + clsSystemRoles.Admin))]
-    public class MemberController : ControllerBase
+    //[Authorize(Roles = (clsSystemRoles.User + "," + clsSystemRoles.Admin))]
+    public class MemberController(IMemberService _memberService) : ControllerBase
     {
-        private readonly IBaseServices<Member> _memberService;
-        private readonly IBaseServices<Person> _personService;
-
-
-        public MemberController(IBaseServices<Member> memberService, IBaseServices<Person> personService)
-        {
-            _memberService = memberService;
-            _personService = personService;
-
-        }
-
-
         [HttpPost]
         [Route("[action]")]
 
         public ActionResult CreateMember(CreateMemberDTO createMemberDTO)
         {
-            //var person = new Person()
-            //{
-            //    Name = createMemberDTO.Name,
-            //    NationalNumber = createMemberDTO.Idcard,
-            //    PhoneNumber = createMemberDTO.PhoneNumber,
-            //    Email = createMemberDTO.Email,
-            //    BirthDate = createMemberDTO.BirthDate
-            //};
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-            
-
-            var member = new Member();
-
-            //member.PersonId = _personService.Add(person);
-            member.MemberWeight = createMemberDTO.MemberWeight;
-            member.IsActive = createMemberDTO.IsActive;
-
-            _memberService.Add(member);
-            return Ok();
-        }
-
-        [HttpPut]
-        [Route("[action]")]
-
-        public ActionResult UpdateMember(UpdateMemberDTO updateMemberDTO)
-        {
-            var existingMember = _memberService.GetById(updateMemberDTO.MemberId);
-            if (existingMember == null)
-                return NotFound();
-
-            //var existingPerson = _personService.GetById(existingMember.PersonId);
-
-            //existingPerson.Name = updateMemberDTO.Name;
-            existingMember.PhoneNumber = updateMemberDTO.PhoneNumber;
-            existingMember.BirthDate = updateMemberDTO.BirthDate;
-            existingMember.Email = updateMemberDTO.Email;
-            //_personService.Update(existingPerson);
-
-            //existingMember.Person = existingPerson;
-            existingMember.MemberWeight = updateMemberDTO.MemberWeight;
-            existingMember.IsActive = updateMemberDTO.IsActive;
-
-            _memberService.Update(existingMember);
-            return Ok();
-        }
-
-
-        [HttpGet]
-        [Route("[action]")]
-        public ActionResult<IEnumerable<ReadMemberDTO>> GetMembers()
-        {
-
-            var members = _memberService.GetAll().Select(member => member.AsDTO());
-
-            return Ok(members);
-        }
-
-        [HttpGet]
-        [Route("[action]/{id}")]
-        public ActionResult<ReadMemberDTO> GetById(int id)
-        {
-            Member member = _memberService.GetById(id);
-            return member is null ? NotFound() : Ok(member.AsDTO());
-        }
-
-        [HttpDelete]
-        [Route("[action]/{id}")]
-        public ActionResult Delete(int id)
-        {
-            Member member = _memberService.GetById(id);
-
-            if (member is null)
-                return NotFound();
-
-            _memberService.DeleteById(member.Id);
+            _memberService.AddNewMember(createMemberDTO.AsMember(),
+                createMemberDTO.SubscriptionInfo.AsSubscription());
 
             return Ok();
         }
+
+        //[HttpPut]
+        //[Route("[action]")]
+
+        //public ActionResult UpdateMember(UpdateMemberDTO updateMemberDTO)
+        //{
+        //    var existingMember = _memberService.GetById(updateMemberDTO.MemberId);
+        //    if (existingMember == null)
+        //        return NotFound();
+
+        //    //var existingPerson = _personService.GetById(existingMember.PersonId);
+
+        //    //existingPerson.Name = updateMemberDTO.Name;
+        //    existingMember.PhoneNumber = updateMemberDTO.PhoneNumber;
+        //    existingMember.BirthDate = updateMemberDTO.BirthDate;
+        //    existingMember.Email = updateMemberDTO.Email;
+        //    //_personService.Update(existingPerson);
+
+        //    //existingMember.Person = existingPerson;
+        //    existingMember.MemberWeight = updateMemberDTO.MemberWeight;
+        //    existingMember.IsActive = updateMemberDTO.IsActive;
+
+        //    _memberService.Update(existingMember);
+        //    return Ok();
+        //}
+
+
+        //[HttpGet]
+        //[Route("[action]")]
+        //public ActionResult<IEnumerable<ReadMemberDTO>> GetMembers()
+        //{
+
+        //    var members = _memberService.GetAll().Select(member => member.AsDTO());
+
+        //    return Ok(members);
+        //}
+
+        //[HttpGet]
+        //[Route("[action]/{id}")]
+        //public ActionResult<ReadMemberDTO> GetById(int id)
+        //{
+        //    Member member = _memberService.GetById(id);
+        //    return member is null ? NotFound() : Ok(member.AsDTO());
+        //}
+
+        //[HttpDelete]
+        //[Route("[action]/{id}")]
+        //public ActionResult Delete(int id)
+        //{
+        //    Member member = _memberService.GetById(id);
+
+        //    if (member is null)
+        //        return NotFound();
+
+        //    _memberService.DeleteById(member.Id);
+
+        //    return Ok();
+        //}
     }
 }
