4a5fc912d90a275a93d60eefd8c316bb7b5fe5b8
Yousef-Binnob <yusufbinnob7111@gmail.com>
2024-10-14 16:58:29 +0300

    refactored checking entities existence in SubscriptionService

--- GymManagementSystem/GymManagement.BusinessCore/Services/SubscriptionService.cs
+++ GymManagementSystem/GymManagement.BusinessCore/Services/SubscriptionService.cs
@@ -6,27 +6,32 @@ using GymManagement.BusinessCore.Models;
 namespace GymManagement.BusinessCore.Services
 {
     public class SubscriptionService(ISubscriptionRepo _subscriptionRepo
-        ,ISubscriptionPeriodRepo _subscriptionPeriodRepo) : ISubscriptionService
+                                    ,ISubscriptionPeriodRepo _subscriptionPeriodRepo
+                                    ,IPeriodRepo _periodRepo
+                                    ,IExerciseTypeRepo _exerciseTypeRepo)
+                                    : ISubscriptionService
     {
         
         public bool AddNewSubscription(Subscription subscription)
         {
-            if ( subscription.ExcerciseTypeId == 0
-                || subscription.PeriodId == 0
-                || subscription.SubscriptionPeriodId == 0) 
-            {
+            var SubscriptionPeriod = _subscriptionPeriodRepo.GetPeriodById(subscription.SubscriptionPeriodId);
+            if (SubscriptionPeriod is null)
                 return false;
-            }
+
+            var Period = _periodRepo.GetPeriodById(subscription.PeriodId);
+            if (Period is null)
+                return false;
+
+            var exerciseType = _exerciseTypeRepo.GetTypeById(subscription.PeriodId);
+            if (exerciseType is null)
+                return false;
+
+
 
             // here must join the subscription with the user create it
             // not implemented yet .....
 
             subscription.StartDate = DateTime.Now;
-            var SubscriptionPeriod = _subscriptionPeriodRepo.GetPeriodById(subscription.PeriodId);
-            if (SubscriptionPeriod == null)
-            {
-                return false;
-            }
             subscription.EndDate = subscription.StartDate.AddDays(SubscriptionPeriod.PeriodDays);
             _subscriptionRepo.AddNewSubscription(subscription);
             return true;
